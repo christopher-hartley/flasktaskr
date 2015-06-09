9149cf13fd403f7c0576e6cf6878b318ea2a133b
Christopher Hartley <christopher.hartley07@gmail.com>
2015-06-08 20:55:49 -0700

    Completed sqlite3 to SQLAlchemy migration.

--- project/views.py
+++ project/views.py
@@ -30,6 +30,7 @@ def login_required(test):
 @app.route('/logout/')
 def logout():
 	session.pop('logged_in', None)
+	session.pop('user_id', None)
 	flash('Goodbye!')
 	return redirect(url_for('login'))
 
@@ -42,6 +43,7 @@ def login():
 			user = User.query.filter_by(name=request.form['name']).first()
 			if user is not None and user.password == request.form['password']:
 				session['logged_in'] = True
+				session['user_id'] = user.id
 				flash('Welcome')
 				return redirect(url_for('tasks'))
 			else:
@@ -64,7 +66,7 @@ def tasks():
 		closed_tasks=closed_tasks
 	)
 
-@app.route('/add/', methods=['POST'])
+@app.route('/add/', methods=['GET', 'POST'])
 @login_required
 def new_task():
 	form = AddTaskForm(request.form)
@@ -76,12 +78,16 @@ def new_task():
 				form.priority.data,
 				datetime.datetime.utcnow(),
 				'1',
-				'1'
+				session['user_id']
 			)
 			db.session.add(new_task)
 			db.session.commit()
 			flash('New entry was successfully posted. Thanks.')
-	return redirect(url_for('tasks'))
+			return redirect(url_for('tasks'))
+		else:
+			flash('All fields are required.')
+			return redirect(url_for('tasks'))
+	return render_template('tasks.html', form=form)
 
 # Mark tasks as complete
 @app.route('/complete/<int:task_id>/')
